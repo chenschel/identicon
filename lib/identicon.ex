0146a652efba405ac9b479fece23dfbcd914081e
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-01-31 21:45:30 +0100

    create a hash from given string and put it into an image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,4 +3,15 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
 end
