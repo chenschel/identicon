ea2e4eb5205b89d0cfd9cfca14c3d1cd435f36f1
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-02-01 13:49:11 +0100

    create the grid with a lot enum functions. add grid struct to image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,22 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image| grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -15,7 +31,7 @@ defmodule Identicon do
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+          |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
   end
